b7dc729a3fc9ca0e606784ee9a2710c0a50e6dad
Kadachi16 <a47541@alunos.ipb.pt>
2026-06-30 20:34:03 +0100

    Challenge 2 - Done

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,17 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
-    // Update is called once per frame
+    // Cria o cooldown e compara com o tempo necessário pro prox cachorro
+    private float cooldown = 2f;
+    private float nextSpawnTime = 0f;
+
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextSpawnTime)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            nextSpawnTime = Time.time + cooldown;
         }
     }
 }
